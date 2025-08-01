d4995739638d6cabc8d1bdd83c50aa2a931ccd44
ash 1913 <ashishganeshshinde@gmail.com>
2025-08-01 17:17:33 +0530

    u

--- src/data/images.jsx
+++ src/data/images.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img1 from '../assets/1.jpg';
+import img1 from '../assets/1.JPG';
 import img2 from '../assets/2.jpg';
 import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
@@ -13,8 +13,8 @@ import img11 from '../assets/11.jpg';
 import img12 from '../assets/12.jpg';
 import img13 from '../assets/13.jpg';
 import img14 from '../assets/14.jpg';
-import img15 from '../assets/15.jpg';
+import img15 from '../assets/15.JPG';
 import img16 from '../assets/16.jpg';
-import img17 from '../assets/17.jpg';
+import img17 from '../assets/17.JPG';
 
 export default [img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,img13,img14,img15,img16,img17];
