17acb1fdd77db2a60ad76e23c18fc9775b07186c
ash 1913 <ashishganeshshinde@gmail.com>
2025-08-03 13:59:25 +0530

    update

--- src/components/FooterSec.jsx
+++ src/components/FooterSec.jsx
@@ -11,6 +11,7 @@ export default function FooterSec(){
                 </div>
                 <div id="copyrightSec">
                     <p>Copyright © 2025 | All Rights Reserved by PridePG Living Pvt Ltd.</p>
+                    <p><a href="mailto:prideliving2@gmail.com?subject=Hello&body=I%20am%20interested%20in%20your%20services." id="mail">prideliving2@gmail.com</a></p>
                     <p id="cpSecPara">Visuals are for reference only. The presence of shown<br />amenities is not assured for every property.</p>
                 </div>
             </footer>
