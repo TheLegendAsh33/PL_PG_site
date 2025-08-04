28357e7329d3a74847a047d51eb77b96a1615208
ash 1913 <ashishganeshshinde@gmail.com>
2025-08-04 10:03:24 +0530

    u

--- src/data/images.jsx
+++ src/data/images.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img1 from '../assets/1.JPG';
+import img1 from '../assets/1.jpg';
 import img2 from '../assets/2.jpg';
 import img3 from '../assets/3.jpg';
 import img4 from '../assets/4.jpg';
