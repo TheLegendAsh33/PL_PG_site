d75574f3719af58c060649195dc2613427d4ec7a
ash 1913 <ashishganeshshinde@gmail.com>
2025-08-04 10:32:04 +0530

    u

--- src/components/ImageCollage.jsx
+++ src/components/ImageCollage.jsx
@@ -1,46 +1,46 @@
-import React, { useState } from "react";
-import Masonry from "react-masonry-css";
-import "../style/ImageCollage.css";
-import images from "../data/images"; // Optional: use an array file or import like you were doing
-import Modal from "./Modal"; // Create this for preview
+// import React, { useState } from "react";
+// import Masonry from "react-masonry-css";
+// import "../style/ImageCollage.css";
+// import images from "../data/images"; // Optional: use an array file or import like you were doing
+// import Modal from "./Modal"; // Create this for preview
 
-const ImageCollage = () => {
-  const [preview, setPreview] = useState(null);
+// const ImageCollage = () => {
+//   const [preview, setPreview] = useState(null);
 
-  const handlePreview = (src) => {
-    setPreview(src);
-  };
+//   const handlePreview = (src) => {
+//     setPreview(src);
+//   };
 
-  const breakpointColumnsObj = {
-    default: 4,
-    1100: 3,
-    700: 2,
-    500: 1
-  };
+//   const breakpointColumnsObj = {
+//     default: 4,
+//     1100: 3,
+//     700: 2,
+//     500: 1
+//   };
 
-  return (
-    <div className="collage-wrapper">
-      <h1><span id="color2">"</span><span id="color1">Woke up to this</span> <span id="color2">Hostel Dreams</span><span id="color1">"</span></h1>
-      <Masonry
-        breakpointCols={breakpointColumnsObj}
-        className="my-masonry-grid"
-        columnClassName="my-masonry-grid_column"
-      >
-        {images.map((src, i) => (
-          <img
-            key={i}
-            src={src}
-            alt={`Collage ${i + 1}`}
-            loading="lazy"
-            className="collage-img"
-            onClick={() => handlePreview(src)}
-          />
-        ))}
-      </Masonry>
+//   return (
+//     <div className="collage-wrapper">
+//       <h1><span id="color2">"</span><span id="color1">Woke up to this</span> <span id="color2">Hostel Dreams</span><span id="color1">"</span></h1>
+//       <Masonry
+//         breakpointCols={breakpointColumnsObj}
+//         className="my-masonry-grid"
+//         columnClassName="my-masonry-grid_column"
+//       >
+//         {images.map((src, i) => (
+//           <img
+//             key={i}
+//             src={src}
+//             alt={`Collage ${i + 1}`}
+//             loading="lazy"
+//             className="collage-img"
+//             onClick={() => handlePreview(src)}
+//           />
+//         ))}
+//       </Masonry>
 
-      {preview && <Modal src={preview} onClose={() => setPreview(null)} />}
-    </div>
-  );
-};
+//       {preview && <Modal src={preview} onClose={() => setPreview(null)} />}
+//     </div>
+//   );
+// };
 
-export default ImageCollage;
+// export default ImageCollage;
